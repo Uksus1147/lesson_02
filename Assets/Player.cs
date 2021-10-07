bcf77f510a0095d51a6128e0da818ab47985bb6e
Uksus1147 <tihomirovdima028@gmail.com>
2021-10-07 15:43:29 +0300

    Домашнее задание к уроку 2 Тихомирова Дмитрия

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 _direct;
+    public float speed = 2;
+    private void Awake()
+    {
+        _direct = Vector3.zero;
+    }
+    private void Update()
+    {
+        _direct.x = Input.GetAxis("Horizontal");
+        _direct.z = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        Vector3 direction = _direct * speed * Time.fixedDeltaTime;
+        transform.Translate(direction);
+    }
+}
